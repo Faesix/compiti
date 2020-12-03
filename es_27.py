1353bd25bf822d5b4819b25fe3c7e3dcceac46f3
Faesix <73987755+Faesix@users.noreply.github.com>
2020-12-03 08:37:38 +0100

    Delete es_27.py

--- es_27.py
+++
@@ -1,9 +0,0 @@
-auto = []
-while True:
-    transito = int(input("Quante auto sono passate oggi? "))
-    if transito == 0:
-        break
-    else:
-        auto.append(transito)
-somma_auto = sum(auto)
-print("Auto transitate: ", somma_auto)
